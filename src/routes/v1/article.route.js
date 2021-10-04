e50e9f25037701b76b115eb5ce60969ceefbcba7
Ramdani Kurnia <adiatma85@gmail.com>
2021-10-04 20:08:43 +0700

    add docs for post articles

--- src/routes/v1/article.route.js
+++ src/routes/v1/article.route.js
@@ -10,3 +10,49 @@ router.route('/').post(validate(articleValidation.createArticle), articleControl
 //   .get(validate(articleValidation.getArticle), articleController.getArticle);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *  name: Articles
+ *  description: Article management and retrieval
+ */
+
+/**
+ * @swagger
+ * /articles:
+ *   post:
+ *     summary: Create a article
+ *     description: Only admins can create article.
+ *     tags: [Articles]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - content
+ *             properties:
+ *               name:
+ *                 type: string
+ *               content:
+ *                  type: string
+ *             example:
+ *               name: Ini judul artikel yang ndak perlu panjang-panjang
+ *               content: Ini konten yang panjang dan lebar hehe
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Article'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
